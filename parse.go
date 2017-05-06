448a868fc13bf450f2b9f6403e76af2580afe70f
Fang Chai <fangbai0330@hotmail.com>
2017-05-06 20:20:21 +0800

    added parse

---
+++ parse.go
@@ -0,0 +1,20 @@
+package parse
+import(
+    "fmt";
+    "github.com/Knetic/govaluate"
+)
+
+
+func Eval(exp string, params map[string]interface{}) (bool,error) {
+    fmt.Println(exp)
+    expression,err := govaluate.NewEvaluableExpression(exp)
+    if err != nil {
+        return false,err
+    }
+
+    result, err := expression.Evaluate(params);
+    return result.(bool),err
+}
+func main(){
+    fmt.Println(Eval("0 < 10", nil))
+}
